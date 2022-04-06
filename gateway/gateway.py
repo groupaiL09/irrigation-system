686fa1d9df714b9ad8909ddf3a3489b9ca0b30dc
Roger <nguyenvo1028rg@gmail.com>
2022-04-06 14:51:54 +0700

    Gateway code - Nguyen

--- gateway/gateway.py
+++ gateway/gateway.py
@@ -1,11 +1,27 @@
+from dataclasses import dataclass
 import serial.tools.list_ports
+import pickle
 import sys
 import time
+from datetime import datetime
 from Adafruit_IO import MQTTClient
 
-ADA_FEED_ID = ["farm1.moisture", "farm1.temperature", "farm1.pump-status"]
+farm_data = {'temp':0, 'moisture': 0 ,'pump_status':"0", 'mode_auto':"0"}
+nextDay = 0
+
+# ADA_FEED_ID = ["farm1.moisture", "farm1.temperature", "farm1.pump-status","farm1.mobile","farm1.mode-auto"]
+ADA_FEED_ID = ["farm1.moisture", "farm1.temperature", "farm1.pump-status","farm1.mobile","farm1.mode-auto"]
 AIO_USERNAME = "groupaiL09"
-AIO_KEY = "aio_vPIj09Q2cV2yVj8uoqnby2UBKCcG"
+AIO_KEY = "aio_bPNS10ry1euFzL4mPL60LfjAnqVE"
+
+
+with open ('./classification_model_pkl.txt','rb') as f:
+    classificationModel = pickle.load(f)
+    
+with open('./regression_model_pkl.txt','rb') as g:
+    regressionModel = pickle.load(g)
+
+min_temp, max_temp, min_moist, max_moist = (30.8, 35.2, 12.79, 68.55)
 
 
 def connected(client):
@@ -28,7 +44,18 @@ def message(client, feed_id, payload):
     if isMicrobitConnected:
         if feed_id == "farm1.pump-status":
             ser.write((str(payload) + "#").encode())
+            farm_data["pump_status"] = str(payload)
             time.sleep(2)
+        if feed_id == "farm1.mobile":
+            if payload == "1":
+                client.publish("farm1.pump-status",farm_data["pump_status"])
+                client.publish("farm1.mode-auto",farm_data["mode_auto"])
+                client.publish("farm1.mobile", nextDay)
+        if feed_id == "farm1.mode-auto":
+            farm_data["mode_auto"] = payload
+        
+
+
 
 
 client = MQTTClient(AIO_USERNAME, AIO_KEY)
@@ -57,25 +84,30 @@ def getPort():
 ser = serial.Serial(port=getPort(), baudrate=115200)
 isMicrobitConnected = True
 
-
-
-
 def processData(data):
-    print(data)
+    print('line 82: ', data)
+    
     # Format for processing data:
     # For temperature: "!1:TEMP:value#"
     # For moisture: "!1:MOIST:value#"
     # For pump: "!1:PUMP:value#"
+
     data = data.replace("!", "")
     data = data.replace("#", "")
     splitData = data.split(":")
+    if len(splitData) != 3:
+        print('line 92', data)
+        return
+
     if splitData[1] == "TEMP":
+        farm_data['temp'] = splitData[2]
         client.publish("farm1.temperature", splitData[2])
-    elif splitData[1] == "MOIST":
+    elif splitData[1] == "MOIST" and len(splitData[2]) != 0:
+        splitData[2] = round(float(int(splitData[2]) / 1024 *100),2)
+        farm_data['moisture'] = splitData[2]
         client.publish("farm1.moisture", splitData[2])
     elif splitData[1] == "PUMP":
         client.publish("farm1.pump-status", splitData[2])
-
 
 mess = ""
 
@@ -94,9 +126,22 @@ def readSerial():
                 mess = mess[end + 1:]
         print(mess)
 
-
 while True:
     readSerial()
-    time.sleep(5)
+    if farm_data["mode_auto"] == "1":
+        print("line 128", type(farm_data["mode_auto"]))
+        new_data = [farm_data['temp'],farm_data['moisture']]
+        decision = classificationModel.predict([new_data])[0]
+        if(str(decision) != farm_data["pump_status"]):
+            farm_data["pump_status"] = str(decision)
+            client.publish("farm1.pump-status",str(decision))
+            if(decision == 0):
+                now = datetime.now()
+                hour = now.hour
+                new_data.append(hour)
+                nextDay = regressionModel.predict([new_data])[0]
+                client.publish("farm1.mobile", str(nextDay))
+        print("line 136", decision)
+    time.sleep(3)
 
 
